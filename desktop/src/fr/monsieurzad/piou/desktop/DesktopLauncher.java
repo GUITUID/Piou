5078b71b939cb2b5e2688b6a54f12e7737aae44d
Guillaume Tuis <guillaume.tuis@gmail.com>
2015-02-09 16:38:53 +0100

    Test

---
+++ desktop/src/fr/monsieurzad/piou/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.monsieurzad.piou.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.monsieurzad.piou.Piou;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Piou(), config);
+	}
+}
